9394bd9ebdbaf321eb63a7577c7641d2c6e5810c
13020180074 <62000267+13020180074@users.noreply.github.com>
2020-03-10 16:03:09 +0800

    Add files via upload

---
+++ TUGAS 1/latihan1.java
@@ -0,0 +1,10 @@
+/*	Nama	:Heryanti
+	tanggal	:10 maret 2020
+	waktu 	:12.55
+*/
+public class latihan1{
+	public static void main(String[]args)
+	{
+	System.out.println("Belajar Bahasa Pemrograman Java");
+	}
+}
---
+++ TUGAS 1/latihan3.java
@@ -0,0 +1,10 @@
+/*	Nama	:Heryanti
+	tanggal	:10 maret 2020
+	waktu 	:12.55
+*/
+public class latihan3{
+	public static void main(String[]args)
+	{
+	System.out.println("Selesai Belajar Pemrograman Web");
+	}
+}
